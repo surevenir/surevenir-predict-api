4130efdfd1f0f7758277fcb7499829656a819c6c
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-12-04 14:13:54 +0800

    fix: hendle prepare image

--- app.py
+++ app.py
@@ -21,5 +21,24 @@ model = tf.keras.models.load_model('./model/model.keras')
 
 app = Flask(__name__)
 
+def prepare_image(img):
+    """
+    Prepares an image for inference by resizing it to the model's required size (224x224) and normalizing its pixel values to be between 0 and 1.
+    
+    Parameters
+    ----------
+    img : PIL.Image
+        The image to be prepared
+    
+    Returns
+    -------
+    np.ndarray
+        A 3D numpy array of shape (1, 224, 224, 3) representing the prepared image
+    """
+    img = img.resize((224, 224))  
+    img_array = np.array(img) / 255.0  
+    img_array = np.expand_dims(img_array, axis=0)  
+    return img_array
+
 if __name__ == '__main__':
     app.run(debug=True)
